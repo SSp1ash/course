34e0592851bc0a6d61c0390af39e677a20a43365
ssp1ash <guoyi3299@live.com>
2021-06-19 17:56:23 +0800

    20210619_001 1.添加eureka 2.添加子项目

---
+++ src/test/java/com/ssp1ash/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssp1ash.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
